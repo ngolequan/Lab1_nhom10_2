997c508a5ed41597949d65435f1a518b786fbab8
Ngo Le Quan <ngolequanit@gmail.com>
2022-01-12 10:56:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/minhhoamaytinh/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.minhhoamaytinh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btncong.setOnClickListener {
+            var kq: Double = (so1.text.toString()).toDouble() + so2.text.toString().toDouble()
+            tvketqua.text = kq.toString()
+        }
+    }
+}
